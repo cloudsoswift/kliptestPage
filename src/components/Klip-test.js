2628ff0f223b5bf750352e277ddc6881e968acfa
cloudsoswift <magic9282m@gmail.com>
2022-01-10 10:12:24 +0900

    update buttons

--- src/components/Klip-test.js
+++ src/components/Klip-test.js
@@ -3,18 +3,20 @@ import { prepare, request, getResult } from 'klip-sdk'
 function KlipTest() {
   const [address, setAddress] = useState(null);
   const [result, setResult] = useState(null); 
+  const [ready, setReady] = useState(false);
+  const [requestKey, setRequestKey] = useState(null);
   const prepare_auth  = async () => {
       const bappName = 'Test BApp'
       const successLink = 'https://www.naver.com'
       const failLink = ''
-      let request_key = null
+      let _request_key = null
       const res = await prepare.auth({ bappName, successLink, failLink }) 
       if(res.err){
           alert("Auth_Error Occured! : " + res.err)
       } else if (res.request_key){
-          request_key = res.request_key
+          _request_key = res.request_key
       }
-      return request_key
+      return _request_key
   }
   const prepare_sendKlay = async () => {
       const bappName = 'Test BApp'
@@ -23,14 +25,14 @@ function KlipTest() {
       const amount = '1'
       const successLink = ''
       const failLink = ''
-      let request_key = null
+      let _request_key = null
       const res = await prepare.sendKLAY({ bappName, from, to, amount, successLink, failLink })
       if (res.err) {
           console.error("SendKlay_Error Occured! : " + res.err)
       } else if (res.request_key){
-          request_key = res.request_key
+          _request_key = res.request_key
       }
-      return request_key
+      return _request_key
   }
   const prepare_executeContract = async () => {
       const bappName = 'Test BApp'
@@ -41,14 +43,14 @@ function KlipTest() {
       const params = "[\"1\"]"
       const successLink = ''
       const failLink = ''
-      let request_key = null
+      let _request_key = null
       const res = await prepare.executeContract({ bappName, from, to, value, abi, params, successLink, failLink })
       if (res.err) {
         alert("executeContract_Error Occured! : " + res.err)
       } else if (res.request_key) {
-        request_key = res.request_key
+        _request_key = res.request_key
       }
-      return request_key
+      return _request_key
   }
   return (
     <div>
@@ -56,15 +58,20 @@ function KlipTest() {
           const req = await prepare_auth()
           alert("req: "+req)
           request(req, ()=> alert('모바일 환경에서 실행하세요.'))
-          setTimeout(async()=>{
-            const real_res = await getResult(req)
+          setRequestKey(req)
+        }}>Auth 버튼</button>
+        {
+          ready ? (<button onClick={
+            async()=>{
+            const real_res = await getResult(requestKey)
             alert("AUTH real result: "+JSON.stringify(real_res))
             alert("주소:" + real_res.result.klaytn_address)
             setAddress(real_res.result.klaytn_address)
             alert("상태:" + real_res.status)
             setResult(real_res.result.status)
-          },5000)
-        }}>Auth 버튼</button>{
+          }}>확인</button>) : (<span/>)
+        }
+        {
           address ? (<span>Address : {address}<br/></span>) : (<span/>)
         }
         {
